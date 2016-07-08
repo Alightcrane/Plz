3702c6c4aff6c61b88b90c79d452e4db73b16e05
Alightcrane <enriquesanchez254@gmail.com>
2016-07-07 20:11:52 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once("Isai.html") ?>
+<?php include_once("Mexico.html") ?>
+<?php include_once("Hunger.html") ?>
+<?php include_once("Role.html") ?>
+<?php include_once("Cancer.html") ?>
+<?php include_once("styles.css") ?>
